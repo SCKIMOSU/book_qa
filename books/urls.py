1bd82d1e08f275fb035d75d88946157155e45aa5
Sang-Chul Kim <sckim7@kookmin.ac.kr>
2025-03-21 15:32:34 +0900

    Add files via upload

---
+++ books/urls.py
@@ -0,0 +1,7 @@
+# books/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+]
---
+++ books/views.py
@@ -0,0 +1,7 @@
+# books/views.py
+from django.shortcuts import render
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/book_list.html', {'books': books})
